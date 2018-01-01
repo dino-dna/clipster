0a86396cddda607ce2b91036078625dc6ab5c7af
cdaringe <christopher.dieringer@gmail.com>
2018-01-01 12:46:53 -0800

    fix(ci): publish osx, 6

---
+++ CONTRIBUTING.md
@@ -0,0 +1,7 @@
+# contributing
+
+## build cycle
+
+the build cycle for this project is a little wonky.  this is because we are bending
+our default project structure to be friendly with `electron-builder`'s defaults and
+`react-scripts` defaults.
--- scripts/semantic-release-extract-next-version.js
+++ scripts/semantic-release-extract-next-version.js
@@ -18,15 +18,15 @@ var childOpts = {
  */
 async function semanticRelease () {
   console.log(`[clipster:semantic-release]: capturing default semantic-release to parse published version`)
-  var stdout = cp.execSync('./node_modules/.bin/semantic-release', childOpts).toString()
+  var stdout = cp.execSync('./node_modules/.bin/semantic-release --dry-run', childOpts).toString()
   process.stdout.write(stdout)
   var version = ''
   // locate the release version
   // ex:
-  //  [Semantic release]: Published release: 0.0.1
+  //  [Semantic release]: The next release version is 0.0.3
   try {
     console.log(`[clipster:semantic-release]: searching for published version`)
-    version = stdout.match(/Published release:\s*([^\s]*)\s*/)[1]
+    version = stdout.match(/The next release version is\s*([^\s]*)\s*/)[1]
     console.log(`[clipster:semantic-release]: found version ${version}`)
   } catch (err) {
     console.log(`[clipster:semantic-release]: version not found`)
