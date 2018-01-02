3787e77c1b711de1a5727af90461f23e193aa4dc
cdaringe <christopher.dieringer@gmail.com>
2018-01-01 20:08:57 -0800

    fix(ci): publish osx, 15

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -3,10 +3,10 @@ npm run lint
 
 if [[ $TRAVIS_BRANCH == "master" ]]
 then
-  npm run build:mac-linux
+  npm run build
   npm run publish
   npm run semantic-release || true
 else
   npx semantic-release --dry-run
-  npm run build:mac-linux
+  npm run build
 fi
