8f8928e2731dd60924309043e2d21841a2d5ae2b
cdaringe <christopher.dieringer@gmail.com>
2018-01-03 00:13:12 -0800

    refactor(ci): use template-ish travis.yml

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -1,12 +1,31 @@
-npm install --verbose
-npm run lint
+yarn
+yarn lint
+yarn build
+
+function electron_build () {
+  if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+    docker run --rm \
+      --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
+      -v ${PWD}:/project \
+      -v ~/.cache/electron:/root/.cache/electron \
+      -v ~/.cache/electron-builder:/root/.cache/electron-builder \
+      electronuserland/builder:wine \
+      /bin/bash -c "yarn --link-duplicates --pure-lockfile && ./node_modules/.bin/electron-builder --linux --win"
+  else
+    ./node_modules/.bin/electron-builder
+  fi
+
+}
 
 if [[ $TRAVIS_BRANCH == "master" ]]
 then
-  npm run build
+  electron_build
   npm run publish
-  npm run semantic-release || true
+  # npm run semantic-release || true
 else
-  npx semantic-release --dry-run
+  electron_build
   npm run build
 fi
+
+# wait for all artifacts up in release
+# close draft
